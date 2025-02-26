135c37ea4be01d851a6303f63abc36a001ef67b0
kimpra2989 <106394183+kimpra2989@users.noreply.github.com>
2025-02-26 23:15:24 +0900

    [level 2] Title: 짝지어 제거하기, Time: 105.15 ms, Memory: 51.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -0,0 +1,11 @@
+function solution(s) {
+    const stack = []
+    
+    for (const c of s) {
+        if (stack.at(-1) == c) {
+            stack.pop()
+        } else stack.push(c)        
+    }
+    
+    return +!stack.length;
+}
