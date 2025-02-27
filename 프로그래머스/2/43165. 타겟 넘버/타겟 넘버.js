9e2efece27ffe29ba7991453680c6c538de185e3
kimpra2989 <106394183+kimpra2989@users.noreply.github.com>
2025-02-27 18:20:50 +0900

    [level 2] Title: 타겟 넘버, Time: 48.73 ms, Memory: 36.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -0,0 +1,16 @@
+function solution(numbers, target) {
+    let count = 0
+    dfs(0, 0)
+    
+    function dfs (idx, sum) {
+        if (idx == numbers.length) {
+            if (sum == target) count++
+            return
+        }
+        
+        dfs(idx + 1, sum + numbers[idx])
+        dfs(idx + 1, sum - numbers[idx])
+    }   
+    
+    return count;
+}
